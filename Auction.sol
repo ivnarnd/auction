bca542b75eecb808a0b17c93f9fad62f86955d63
ivan <iaranda264@gmail.com>
2025-06-11 19:59:56 -0300

    estructura inicial de la subasta

--- Auction.sol
+++ Auction.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 contract Auction{
+
     address owner;
-    constructor(){
+    uint256 currentOffer;
+    address currentBidder;
+    mapping (address => uint256) offers;
+
+    constructor(uint256 _initialOffer){
         owner = msg.sender;
+        currentOffer = _initialOffer;
+        
+    }
+
+    function offer() payable external{
+        require(msg.value > (currentOffer + (currentOffer * 5 /100)));
+        offers[msg.sender] = msg.value;
+        currentBidder =  msg.sender;
+        currentOffer = msg.value;
     }
 }
